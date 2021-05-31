c58cf836aead135fb8dd974c93e27906469d079c
superco-art <daniela.trifu@gmail.com>
2021-05-31 15:37:01 +0200

    Initial commit

---
+++ src/test/java/org/factoriaf5/libritos/LibritosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.libritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibritosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
